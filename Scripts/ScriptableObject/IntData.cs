bb176e46813949c34a87a713c6ce39aa5653a39a
Will Lemmons <lemonwill.three@gmail.com>
2022-09-01 12:34:20 -0600

    Scriptable

---
+++ Scripts/ScriptableObject/IntData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value = 1;
+
+    public void ChangeValue(int number)
+    {
+        value += number;
+    }
+    
+}
